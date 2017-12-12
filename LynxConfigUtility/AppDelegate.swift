2fc1b62c31e15087ec76d1760571265f154fca8b
Richard Hodge <richardj@hodgefamily.net>
2017-12-12 16:22:57 -0600

    Initial Project Setup

---
+++ LynxConfigUtility/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LynxConfigUtility
+//
+//  Created by Richard Hodge on 12/12/17.
+//  Copyright © 2017 Binary Platypus Software. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
